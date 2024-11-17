e05c306babb6c431e442cb57937f134f91aee2c4
2477carlos.a <2477carlos.a@gmail.com>
2024-11-17 05:12:37 +0000

    añadir swagger al servidor

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const sequelize = require("./config/db");
+const homeRoutes = require('./routes/home')
+const swaggerUi = require('swagger-ui-express')
+const swaggerSpec = require('./swagger')
 
 const app = express();
 const PORT = 8000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', homeRoutes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 // conectar base de datos
 if(sequelize){
@@ -10,18 +20,15 @@ if(sequelize){
       .sync({ foce: false })
       .then(() => {
         app.listen(PORT, ()=>{
-            console.clear()
             console.log("Server running on http://localhost:"+PORT)
             console.log("hot reload is working 👍")
         })
       })
       .catch((error) => {
-        console.clear();
         console.error(error);
         console.log("Could not connect database 💀❌");
       });
 }else{
-    console.clear()
     console.log("WRANING!!!")
     console.log("DB_URL is not set")
     console.log("please configure you environment variables correctly")
