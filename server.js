f07df0593f45787197d311b98d77de356cbe4df7
xs_ismand3ss <2477carlos.a@gmail.com>
2024-11-16 21:05:46 -0600

    configurar servidor

---
+++ server.js
@@ -0,0 +1,30 @@
+// import express from 'express'
+// import sequelize from './config/db.js'
+const express = require("express");
+const sequelize = require("./config/db");
+
+const app = express();
+const PORT = 8000;
+
+// conectar base de datos
+if(sequelize){
+    sequelize
+      .sync({ foce: false })
+      .then(() => {
+        app.listen(PORT, ()=>{
+            console.clear()
+            console.log("Server running on http://localhost:"+PORT)
+            console.log("hot reload is working 👍")
+        })
+      })
+      .catch((error) => {
+        console.clear();
+        console.error(error);
+        console.log("Could not connect database 💀❌");
+      });
+}else{
+    console.clear()
+    console.log("WRANING!!!")
+    console.log("DB_URL is not set")
+    console.log("please configure you environment variables correctly")
+}
